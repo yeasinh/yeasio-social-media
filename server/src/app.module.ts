c1c1519129762f02f8a287efc6250d9411c7a031
Muhammad Yeasin Hossain <yeasinhossain.246@gmail.com>
2025-07-02 15:15:50 +0600

    Initial commit: setup monorepo structure

---
+++ server/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { PrismaModule } from 'prisma/prisma.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    PrismaModule,
+  ],
+})
+export class AppModule {}
